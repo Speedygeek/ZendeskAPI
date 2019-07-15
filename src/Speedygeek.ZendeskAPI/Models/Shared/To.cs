807bd8d0060d6bf6a26057e8036dd02d83cb970d
Elizabeth Schneider <elizabeth@speedy-geek.com>
2019-07-14 23:09:35 -0600

    add more Via

---
+++ src/Speedygeek.ZendeskAPI/Models/Shared/To.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Elizabeth Schneider. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Speedygeek.ZendeskAPI.Models
+{
+    public class To
+    {
+        /// <summary>
+        /// Address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// set when channel is voice
+        /// </summary>
+        public string FormattedPhone { get; set; }
+
+        /// <summary>
+        /// set when channel is voice
+        /// </summary>
+        public string Phone { get; set; }
+    }
+}
